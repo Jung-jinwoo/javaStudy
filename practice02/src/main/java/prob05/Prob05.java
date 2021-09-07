765c0d2ef31b98b4bb61dfec43551fb8cf7b461c
jung-jinwoo <wlsdn960319@gmail.com>
2021-09-07 18:00:29 +0900

    practice02 완료

--- practice02/src/main/java/prob05/Prob05.java
+++ practice02/src/main/java/prob05/Prob05.java
@@ -8,28 +8,45 @@ public class Prob05 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
 
-		System.out.println("수를 결정하였습니다. 맞추어 보세요");
-		
 		while( true ) {
 			
+			int small = 1;
+			int big = 100;
+			System.out.println("수를 결정하였습니다. 맞추어 보세요");
+			
 			/* 게임 작성 */
-
+			System.out.println(small + "-" + big);
 			// 정답 램덤하게 만들기
 			Random random = new Random();
 			int correctNumber = random.nextInt( 100 ) + 1;
 			System.out.println(correctNumber);
 
-			System.out.println();
-			
-			//새 게임 여부 확인하기
+			for(int i=1; i>0; i++) {
+				System.out.print(i + ">>");
+				int check = scanner.nextInt();
+				if(correctNumber > check) {
+					System.out.println("더 높게");
+					small = check;
+					continue;
+				}else if(correctNumber < check) {
+					System.out.println("더 낮게");
+					big = check;
+					continue;
+				}else if(correctNumber == check){
+					System.out.println("맞았습니다");
+					break;
+				}
+			}
 			System.out.print( "다시 하겠습니까(y/n)>>" );
 			String answer = scanner.next();
 			if( "y".equals( answer ) == false ) {
 				break;
 			}
 		}
-		
+		//새 게임 여부 확인하기
+		System.out.println("게임이 종료되었습니다.");
 		scanner.close();
+		
 	}
 
 }
