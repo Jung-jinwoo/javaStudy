823540380f3e0bbf32b791a90affeec9146a3120
jung-jinwoo <wlsdn960319@gmail.com>
2021-09-10 17:01:52 +0900

    String 연습

---
+++ chapter04/src/main/java/chapter04/Rect.java
@@ -0,0 +1,20 @@
+package chapter04;
+
+public class Rect {
+
+	private int width;
+	private int height;
+	
+	public Rect(int width, int height) {
+		super();
+		this.width = width;
+		this.height = height;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		return super.equals(obj);
+	}
+	
+}
