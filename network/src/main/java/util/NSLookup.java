6cc33384030d08ded48114d1b2d02860298c28e2
jung-jinwoo <wlsdn960319@gmail.com>
2021-09-16 16:04:58 +0900

    NSLookup 완료

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -2,19 +2,25 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 
 	public static void main(String[] args) {
-		String line = "www.douzone.com";
-		
+		Scanner sc = new Scanner(System.in);
+		String line = sc.nextLine();
+		String[] str;
 		try {
 			InetAddress[] inetAddresses = InetAddress.getAllByName(line);// 이름으로 모든 ip를 가져옴
 			
+			for(int i=0; i<inetAddresses.length; i++) {
+				System.out.println(inetAddresses[i].toString().split("/")[1]);
+			}
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} 
 
 	}
+	
 
 }
